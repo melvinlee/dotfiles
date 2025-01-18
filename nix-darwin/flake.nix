5731f2d3d5c464fbd41ff57d26f74fdc3e262537
melvinlee <melvinlee@hotmail.com>
2025-01-18 08:24:40 +0800

    added spacebar configuration.

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -19,6 +19,16 @@
           pkgs.nushell
         ];
 
+      services.spacebar = {
+        enable = true;
+        package = pkgs.spacebar;
+        config = {
+          clock_format     = "%R";
+          background_color = "0xff202020";
+          foreground_color = "0xffa8a8a8";
+        };
+      };
+
       services.nix-daemon.enable = true;
       nix.settings.experimental-features = "nix-command flakes";
 
