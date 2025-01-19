669778a2cd66a1ce3ffdcbe27c89408442b8f646
melvinlee <melvinlee@hotmail.com>
2025-01-19 22:10:01 +0800

    added more packages.

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -8,11 +8,18 @@
   home.homeDirectory = "/Users/melvinlee";
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
-# Makes sense for user specific applications that shouldn't be available system-wide
+  nixpkgs.config.allowUnfree = true;
+
+  # Makes sense for user specific applications that shouldn't be available system-wide
   home.packages = with pkgs;[
-    httpie
-    kind
-    checkov
+    httpie # HTTPie is a command line HTTP client
+    kind # Kubernetes IN Docker - local clusters for testing Kubernetes
+    checkov # Infrastructure as Code Static Analysis
+    kubernetes-helm # The Kubernetes Package Manager
+    helmfile # Deploy Kubernetes Helm Charts
+    k9s # Kubernetes CLI To Manage Your Clusters In Style!
+    bat # A cat(1) clone with wings. https://github.com/sharkdp/bat
+    cmatrix # cmatrix is a curses based program that simulates the display from "The Matrix"
   ];
 
   home.file = {
@@ -28,6 +35,6 @@
     "$HOME/.nix-profile/bin"
   ];
 
-  # programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
 }
