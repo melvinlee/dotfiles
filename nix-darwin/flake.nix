bd2975c8e1cce752c23c20732d160657530da680
melvinlee <melvinlee@hotmail.com>
2025-02-06 09:47:47 +0800

    add ollama

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -37,6 +37,7 @@
             enable = true;
             brews = [
               "stow" # GNU Stow is a symlink farm manager which takes distinct packages of software and/or data located in separate directories on the filesystem, and makes them appear to be installed in the same place.
+              "ollama" 
             ];
             casks = [
               "ghostty" # A simple and lightweight tool for creating and managing SSH tunnels
